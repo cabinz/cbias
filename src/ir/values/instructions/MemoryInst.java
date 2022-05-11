67823a1555ff849c091734bea59c231ab5ef8d9b
cabinz <1597405094@qq.com>
2022-05-12 01:01:22 +0800

    Add Alloca to MemoryInst
    - Add nested class MemoryInst.Alloca

--- src/ir/values/instructions/MemoryInst.java
+++ src/ir/values/instructions/MemoryInst.java
@@ -3,14 +3,49 @@ package ir.values.instructions;
 import ir.Type;
 import ir.Value;
 import ir.types.IntegerType;
+import ir.types.PointerType;
 import ir.values.Instruction;
 
 /**
- * This class contains nested classes represent instructions that are memory-related.
- * In LLVM IR, these instructions inherit from Instruction (Store)
+ * This class contains nested classes represent instructions that are memory-related,
+ * including memory accessing and addressing instructions.
+ * In the latest version of LLVM IR, these instructions inherit from Instruction (Store)
  * or Instruction.UnaryInstruction (Load, Alloca, ZExt).
  */
 public class MemoryInst {
+
+    /**
+     * Alloca is an instruction to allocate memory on the stack,
+     * yielding an address (pointer) where the memory allocated
+     * lands as result.
+     * <br>
+     * Type for Alloca is PointerType.
+     * @see <a href="https://github.com/hdoc/llvm-project/blob/release/13.x/llvm/include/llvm/IR/Instructions.h#L62">
+     *     LLVM IR Source: AllocaInst</a>
+     * @see <a href="https://llvm.org/docs/LangRef.html#alloca-instruction">
+     *     LLVM Language Reference: Alloca Instrucstion</a>
+     */
+    public static class Alloca extends Instruction {
+        //<editor-fold desc="Fields">
+        public final Type allocatedType;
+//        private boolean isInit = false;
+        //</editor-fold>
+
+
+        //<editor-fold desc="Constructors">
+        public Alloca(Type allocatedType) {
+            super(PointerType.getType(allocatedType), InstCategory.ALLOCA, 0);
+            this.allocatedType = allocatedType;
+        }
+        //</editor-fold>
+
+        //<editor-fold desc="Methods">
+        @Override
+        public String toString() {
+            return this.name + " = " + "alloca " + allocatedType.toString();
+        }
+        //</editor-fold>
+    }
 
     /**
      * Represents Zero Extension of integer type.
