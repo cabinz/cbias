e823196c05e2da7386255f2610d2c47c11d06206
LIINXK <1264508018@qq.com>
2022-07-25 13:11:03 +0800

    [Update] Add FPneg

---
+++ src/backend/armCode/MCInstructions/MCFPneg.java
@@ -0,0 +1,40 @@
+package backend.armCode.MCInstructions;
+
+import backend.armCode.MCInstruction;
+import backend.operand.ExtensionRegister;
+import backend.operand.Register;
+
+import java.util.HashSet;
+
+public class MCFPneg extends MCInstruction {
+
+    private ExtensionRegister destination;
+    private ExtensionRegister operand;
+
+    @Override
+    public HashSet<Register> getDef() {
+        return null;
+    }
+
+    @Override
+    public HashSet<Register> getUse() {
+        return null;
+    }
+
+    @Override
+    public void replaceRegister(Register old, Register tmp) {
+
+    }
+
+    @Override
+    public String emit() {
+        return "VNEG" + emitCond() + ".F32 " + destination.emit()
+                + ", " + operand.emit();
+    }
+
+    public MCFPneg(ExtensionRegister destination, ExtensionRegister operand) {
+        super(TYPE.VNEG);
+        this.destination = destination;
+        this.operand = operand;
+    }
+}
