7faedde6d8034bbd472ae6517ef40eea183d5f3b
omg_link <2739614234@qq.com>
2022-07-31 16:13:18 +0800

    [Fix] Create method User.clearOperands to clear operands correctly.
    [Fix] Create method User.clearOperands to clear operands correctly.

--- src/passes/ir/dce/UnreachableCodeElim.java
+++ src/passes/ir/dce/UnreachableCodeElim.java
@@ -47,47 +47,13 @@ public class UnreachableCodeElim implements IRPass {
             var followedBlocks = RelationAnalysis.getFollowingBB(basicBlock);
             RelationAnalysis.freeBlock(basicBlock);
             for (BasicBlock followedBlock : followedBlocks) {
-                RelationAnalysis.removeEntry(followedBlock,basicBlock);
+                if(!removeSet.contains(followedBlock)){
+                    RelationAnalysis.removeEntry(followedBlock,basicBlock);
+                }
             }
         }
         for (BasicBlock basicBlock : removeSet) {
             basicBlock.markWasted();
-        }
-    }
-
-    /**
-     * Remove blocks with no entry.
-     * <p> - No longer used since the other optimize contains this.
-     * <p> - But why don't I delete this?
-     * <p> - I don't know :(
-     */
-    private static void removeNoEntryBlocks(Function function) {
-        // Maybe we should use RelationAnalysis here.
-        Map<BasicBlock, Set<BasicBlock>> entryMap = new HashMap<>();
-        function.forEach(basicBlock -> entryMap.put(basicBlock, new HashSet<>()));
-        function.forEach(basicBlock -> {
-            var followingBBs = RelationAnalysis.getFollowingBB(basicBlock);
-            followingBBs.forEach(followingBB-> entryMap.get(followingBB).add(basicBlock));
-        });
-        Queue<BasicBlock> removeQueue = new ArrayDeque<>();
-        entryMap.forEach((basicBlock, entrySet) -> {
-            if(basicBlock!= function.getEntryBB()&&entrySet.isEmpty()){
-                removeQueue.add(basicBlock);
-            }
-        });
-        while (!removeQueue.isEmpty()){
-            var basicBlock = removeQueue.remove();
-            var followingBBs = RelationAnalysis.getFollowingBB(basicBlock);
-            entryMap.remove(basicBlock);
-            RelationAnalysis.freeBlock(basicBlock); // Free operands
-            for (BasicBlock followingBB : followingBBs) {
-                entryMap.get(followingBB).remove(basicBlock);
-                RelationAnalysis.removeEntry(followingBB, basicBlock);
-                if(entryMap.get(followingBB).isEmpty()){
-                    removeQueue.add(followingBB);
-                }
-            }
-            basicBlock.markWasted(); // Mark wasted
         }
     }
 
