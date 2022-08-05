1796ded0ae073073249d70a0f96931935539ce13
LIINXK <1264508018@qq.com>
2022-08-05 16:14:52 +0800

    [Update] MergeBlock deletes the last branch now
    - Delete the last branch of one block and order the successor behind the block if possible
    - Sort the MCBB in the order above, which may cause the block index disorder

--- src/passes/mc/mergeBlock/MergeBlock.java
+++ src/passes/mc/mergeBlock/MergeBlock.java
@@ -3,24 +3,37 @@ package passes.mc.mergeBlock;
 import backend.ARMAssemble;
 import backend.armCode.MCBasicBlock;
 import backend.armCode.MCFunction;
+import backend.armCode.MCInstructions.MCReturn;
+import backend.armCode.MCInstructions.MCbranch;
 import passes.mc.MCPass;
 
-import java.util.HashSet;
+import java.util.LinkedList;
 
 public class MergeBlock implements MCPass {
 
     @Override
     public void runOnModule(ARMAssemble armAssemble) {
         for (MCFunction func : armAssemble) {
-            var visited = new HashSet<MCBasicBlock>();
-            serialize(func.getEntryBlock(), func.getEntryBlock(), visited);
+            var new_list = new LinkedList<MCBasicBlock>();
+            mergeBlock(func.getEntryBlock(), new_list);
+            func.setBasicBlockList(serialize(new_list));
         }
     }
 
-    public void serialize(MCBasicBlock dealing, MCBasicBlock insert, HashSet<MCBasicBlock> visited) {
-        if (visited.contains(dealing))
+    private void mergeBlock(MCBasicBlock dealing, LinkedList<MCBasicBlock> new_list) {
+        mergeBlock(dealing, dealing, new_list);
+    }
+
+    /**
+     * Merge block and sort the basic block by DFS order
+     * @param dealing the basic block to be analysed
+     * @param insert the block which merged block will be inserted to
+     * @param new_list the reordered list by DFS
+     */
+    private void mergeBlock(MCBasicBlock dealing, MCBasicBlock insert, LinkedList<MCBasicBlock> new_list) {
+        if (new_list.contains(dealing))
             return;
-        visited.add(dealing);
+        new_list.add(dealing);
 
         /* Meaning it's a return block */
         if (dealing.getTrueSuccessor() == null)
@@ -30,37 +43,80 @@ public class MergeBlock implements MCPass {
         if (dealing.getFalseSuccessor() == null) {
             var onlySucc = dealing.getTrueSuccessor();
             if (onlySucc.getPredecessors().size() == 1) {
-                insert.getInstructionList().removeLast();
+                insert.removeLast();
                 insert.appendAndRemove(onlySucc);
                 insert.setTrueSuccessor(onlySucc.getTrueSuccessor());
                 insert.setFalseSuccessor(onlySucc.getFalseSuccessor());
-                serialize(insert, insert, visited);
+                mergeBlock(insert, insert, new_list);
             }
             else
-                serialize(onlySucc, onlySucc, visited);
+                mergeBlock(onlySucc, onlySucc, new_list);
         }
         else {
             var trueSucc = dealing.getTrueSuccessor();
             var falseSucc = dealing.getFalseSuccessor();
             if (trueSucc.getPredecessors().size() == 1) {
-                var list = insert.getInstructionList();
-                list.remove(list.size()-2);
+                insert.removeSecondLast();
                 insert.appendAndRemove(trueSucc);
                 insert.setTrueSuccessor(trueSucc.getTrueSuccessor());
-                serialize(trueSucc, insert, visited);
-                serialize(falseSucc, falseSucc, visited);
+                mergeBlock(trueSucc, insert, new_list);
+                mergeBlock(falseSucc, falseSucc, new_list);
             }
             else if (falseSucc.getPredecessors().size() == 1){
-                insert.getInstructionList().removeLast();
+                insert.removeLast();
                 insert.appendAndRemove(falseSucc);
                 insert.setTrueSuccessor(trueSucc.getTrueSuccessor());
-                serialize(falseSucc, insert, visited);
-                serialize(trueSucc, trueSucc, visited);
+                mergeBlock(falseSucc, insert, new_list);
+                mergeBlock(trueSucc, trueSucc, new_list);
             }
             else {
-                serialize(trueSucc, trueSucc, visited);
-                serialize(falseSucc, falseSucc, visited);
+                mergeBlock(trueSucc, trueSucc, new_list);
+                mergeBlock(falseSucc, falseSucc, new_list);
             }
         }
+    }
+
+    private LinkedList<MCBasicBlock> serialize(LinkedList<MCBasicBlock> list) {
+        var serializedList = new LinkedList<MCBasicBlock>();
+        while (!list.isEmpty()) {
+            /* Following the DFS order for the longest branch chain */
+            MCBasicBlock dealing = list.getFirst();
+            while (dealing != null) {
+                serializedList.add(dealing);
+                list.remove(dealing);
+
+                if (dealing.getLastInst() instanceof MCReturn) {
+                    break;
+                }
+                else {
+                    var branch = ((MCbranch) dealing.getLastInst());
+                    assert branch.isBranch();
+                    if (branch.getCond() == null) {
+                        var target = branch.getTargetBB();
+                        if (serializedList.contains(target))
+                            break;
+                        else {
+                            dealing.removeLast();
+                            dealing = target;
+                        }
+                    }
+                    else {
+                        var trueBlock = ((MCbranch) dealing.getSecondLastInst()).getTargetBB();
+                        var falseBlock = ((MCbranch) dealing.getLastInst()).getTargetBB();
+                        if (!serializedList.contains(trueBlock)) {
+                            dealing.removeSecondLast();
+                            dealing = trueBlock;
+                        }
+                        else if (!serializedList.contains(falseBlock)){
+                            dealing.removeLast();
+                            dealing = falseBlock;
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+        }
+        return serializedList;
     }
 }
