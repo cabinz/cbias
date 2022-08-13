9b8c268d25e93941a935750ce7bc221aa34e8e6c
LIINXK <1264508018@qq.com>
2022-08-13 11:38:00 +0800

    [Fix] Better convert

--- src/backend/MCBuilder.java
+++ src/backend/MCBuilder.java
@@ -272,12 +272,6 @@ public class MCBuilder {
         else if (valueMap.containsKey(value)) {
             return valueMap.get(value);
         }
-        else if (floatValueMap.containsKey(value)) {
-            var vr = curFunc.createVirReg(value);
-            valueMap.put(value, vr);
-            curMCBB.appendInst(new MCFPmove(vr, floatValueMap.get(value)));
-            return vr;
-        }
         else if (value instanceof Instruction) {
             var inst = ((Instruction) value);
             VirtualRegister vr;
@@ -371,11 +365,6 @@ public class MCBuilder {
                 return extVr;
             }
         }
-        else if (valueMap.containsKey(value)) {
-            var extVr = curFunc.createExtVirReg(value);
-            curMCBB.appendInst(new MCFPmove(extVr, valueMap.get(value)));
-            return extVr;
-        }
         else if (floatValueMap.containsKey(value)) {
             return floatValueMap.get(value);
         }
@@ -383,15 +372,6 @@ public class MCBuilder {
             var inst = ((Instruction) value);
             var extVr = curFunc.createExtVirReg(inst);
             floatValueMap.put(value, extVr);
-            /* If a phi value is going to be converted, the condition below will be true */
-            /* At the moment, there is NOT a corresponding core register container for phi. */
-            /* And then every thing is going wrong! */
-            /* Create a core register for phi and then VMOV to solve */
-            if (inst.isPhi() && !inst.getType().isFloatType()) {
-                var vr = curFunc.createVirReg(inst);
-                valueMap.put(inst, vr);
-                curMCBB.appendInst(new MCFPmove(extVr, vr));
-            }
             return extVr;
         }
         else if (value instanceof Function.FuncArg && curIRFunc.getArgs().contains(value)) {
@@ -1015,14 +995,18 @@ public class MCBuilder {
      * sitofp needs to insert VMOV from core register, while fptosi needs to VMOV from extReg back
      */
     private void translateConvert(CastInst IRinst, boolean f2i) {
+        var operand = IRinst.getOperandAt(0);
+        if (!f2i) {
+            curMCBB.appendInst(new MCFPmove((ExtensionRegister) findFloatContainer(operand),(Register) findContainer(operand)));
+        }
         curMCBB.appendInst(new MCFPconvert(
                 f2i,
                 (ExtensionRegister) findFloatContainer(IRinst),
-                (ExtensionRegister) findFloatContainer(IRinst.getOperandAt(0))
+                (ExtensionRegister) findFloatContainer(operand)
         ));
-        /* Move it back to core register */
-        if (f2i)
-            findContainer(IRinst);
+        if (f2i) {
+            curMCBB.appendInst(new MCFPmove((Register) findContainer(IRinst),(ExtensionRegister) findFloatContainer(IRinst)));
+        }
     }
 
     /**
